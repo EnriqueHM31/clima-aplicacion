101de0d2cd980988f253771c85de2eaf3586b47d
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-12 13:56:34 -0600

    Add obtenerClimaLugar function to fetch weather data for a specified location and number of days, enhancing the weather service capabilities.

---
+++ src/services/Clima.ts
@@ -0,0 +1,16 @@
+// src/services/clima.service.ts
+export async function obtenerClimaLugar(
+    lugar: string,
+    days: number
+) {
+    const url = `${import.meta.env.VITE_API}q=${lugar}&days=${days}&aqi=yes&alerts=no`;
+    console.log({ url });
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error("Error al obtener el clima");
+    }
+
+    return response.json();
+}
