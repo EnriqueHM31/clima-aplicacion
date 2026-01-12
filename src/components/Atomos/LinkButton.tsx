31fc989e8953c8cbaeb2123aac6d1c19e614ac4e
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-11 22:34:34 -0600

    Refactor LinkButton component to utilize Framer Motion for animations, enhancing visual appeal and interactivity. Added support for motion variants to customize animation behavior.

--- src/components/Atomos/LinkButton.tsx
+++ src/components/Atomos/LinkButton.tsx
@@ -1,14 +1,46 @@
+import { motion } from "framer-motion";
+import type { Target, Transition } from "framer-motion";
 
-interface PropsLinkButton {
-    text: string
-    icon: React.ReactNode
+export interface MotionVariant {
+    initial: Target;
+    animate: Target;
+    transition?: Transition;
 }
 
-export default function LinkButton({ text, icon }: PropsLinkButton) {
+interface PropsLinkButton {
+    text: string;
+    icon: React.ReactNode;
+    variants: MotionVariant;
+}
+
+export default function LinkButton({ text, icon, variants }: PropsLinkButton) {
     return (
-        <a href="https://github.com/EnriqueHM31?tab=repositories" target="_blank" className="bg-white text-blue-950 px-4 py-2 rounded-md font-bold flex items-center gap-2 cursor-pointer hover:bg-blue-700 transition-all duration-300 hover:text-white ease-in">
-            <p>{text}</p>
+        <motion.a
+            href="https://github.com/EnriqueHM31?tab=repositories"
+            target="_blank"
+            className="
+        bg-white
+        text-blue-950
+        px-4
+        py-2
+        rounded-md
+        font-bold
+        flex
+        items-center
+        gap-2
+        cursor-pointer
+        hover:bg-blue-700
+        hover:text-white
+        transition-all
+        duration-300
+        ease-in
+      "
+            initial={variants.initial}
+            animate={variants.animate}
+            transition={variants.transition}
+        >
+            <span>{text}</span>
             {icon}
-        </a>
-    )
+        </motion.a>
+    );
 }
