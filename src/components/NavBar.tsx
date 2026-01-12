9d884e36c363f7f527542d6695851dfda190159e
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-11 22:11:20 -0600

    Update NavBar component to improve layout responsiveness with flexbox adjustments and added spacing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
 
     return (
-        <nav className="flex justify-between items-center py-1  w-full max-w-7xl mx-auto">
+        <nav className="flex md:flex-row flex-col justify-between items-center py-1  w-full max-w-7xl mx-auto gap-4 mb-4">
             <Link to="/" className="flex items-center gap-4">
                 <picture className="size-12 bg-white rounded-full p-2 object-cover flex items-center justify-center">
                     <img src={ICONO} alt="logo" className=" object-cover" />
