cf96b56027688b4d7f70b7d871cf39799b3fd963
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-13 22:07:24 -0600

    Refactor LugarClima component to enhance state management and data fetching. Integrate SearchClima for user input and update effect dependencies for improved functionality.

--- src/pages/LugarClima.tsx
+++ src/pages/LugarClima.tsx
@@ -4,31 +4,32 @@ import { useParams, useSearchParams } from "react-router-dom";
 import LoaderGrid from "../components/Atomos/Loading";
 import LocationInfoBar from "../components/ClimaPage/DataLocation";
 import ClimaLugarPage from "../components/ClimaPage/Sections/ClimaLugar";
-import { obtenerClimaLugar } from "../services/Clima";
+//import { obtenerClimaLugar } from "../services/Clima";
 import { useClimaStore } from "../store/Clima";
+import { InfoClima } from "../data/Clima";
+import SearchClima from "../components/Atomos/Search";
 
 export default function LugarClima() {
-    const { lugar } = useParams<{ lugar: string }>();
     const [searchParams] = useSearchParams();
-    const { obtenerClima, dataClima } = useClimaStore();
+    const { obtenerClima, dataClima, handleChangeDias, handleChangeCiudad } = useClimaStore();
 
-
-    const days = Number(searchParams.get("days") ?? 1);
-
-    console.log({ days });
-
+    const { lugar } = useParams<{ lugar: string }>();
     useEffect(() => {
+        const days = Number(searchParams.get("days") ?? 0);
+        handleChangeDias(days);
+        handleChangeCiudad(lugar ?? "");
+
         if (!lugar) return;
 
         const fetchData = async () => {
-            const data = await obtenerClimaLugar(lugar, days);
-            //const data = InfoClima;
+            //const data = await obtenerClimaLugar(lugar, days);
+            const data = InfoClima;
             console.log({ data });
             setTimeout(() => obtenerClima(data), 2000);
         };
 
         fetchData();
-    }, [lugar, days, obtenerClima]);
+    }, [lugar, obtenerClima, handleChangeDias, searchParams, handleChangeCiudad]);
 
     return (
         <section className="w-full text-white">
@@ -36,6 +37,9 @@ export default function LugarClima() {
                 !dataClima ? <LoaderGrid /> :
                     <main className="w-full ">
                         <LocationInfoBar location={dataClima.location} />
+                        <div className="my5 flex flex-col gap-4 justify-end items-end max-w-7xl mx-auto">
+                            <SearchClima />
+                        </div>
                         <ClimaLugarPage data={dataClima} />
                     </main>
             }
