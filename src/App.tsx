7d4054ae59e1aeb38e31a4666b4156f1adb9efc1
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-14 00:20:37 -0600

    Enhance App component: add Toaster for notifications, improve code formatting, and maintain consistent import style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import Fondo from './components/Landing/Fondo'
-import AppRouter from './routes/routes'
+import Fondo from "./components/Landing/Fondo";
+import AppRouter from "./routes/routes";
+import { Toaster } from "sonner";
 
 function App() {
-
-  return (
-    <>
-      <div className=''>
-        <Fondo>
-          <AppRouter />
-        </Fondo>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="">
+                <Fondo>
+                    <AppRouter />
+                </Fondo>
+            </div>
+            <Toaster
+                expand
+                position="bottom-right"
+                toastOptions={{
+                    className: "bg-blue-950 text-white border border-blue-800",
+                    style: {
+                        background: "#000",
+                        color: "white",
+                        border: "1px solid #48e",
+                        borderRadius: "10px",
+                    },
+                }}
+            />
+        </>
+    );
 }
 
-export default App
+export default App;
