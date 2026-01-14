0c1e02e3c005d6b3e331ee4d4af5bdfb4043d915
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-14 00:20:18 -0600

    Implement error handling in LugarClima component: add navigation and toast notifications for API errors, and update useEffect dependencies for improved functionality.

--- src/pages/LugarClima.tsx
+++ src/pages/LugarClima.tsx
@@ -1,19 +1,24 @@
 // src/pages/ClimaLugar.tsx
 import { useEffect } from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import LoaderGrid from "../components/Atomos/Loading";
 import LocationInfoBar from "../components/ClimaPage/DataLocation";
 import ClimaLugarPage from "../components/ClimaPage/Sections/ClimaLugar";
 //import { obtenerClimaLugar } from "../services/Clima";
 import SearchClima from "../components/Atomos/Search";
 import { useClimaStore } from "../store/Clima";
+import { toast } from "sonner";
 
 export default function LugarClima() {
     const [searchParams] = useSearchParams();
-    const { dataClima, handleChangeDias, handleChangeCiudad, obtenerClimaLugar } = useClimaStore();
-
+    const { dataClima, handleChangeDias, handleChangeCiudad, obtenerClimaLugar, error } = useClimaStore();
     const { lugar } = useParams<{ lugar: string }>();
+    const navigate = useNavigate();
     useEffect(() => {
+        if (error) {
+            navigate("/");
+            toast.error(error);
+        }
         const days = Number(searchParams.get("days") ?? 0);
         handleChangeDias(days);
         handleChangeCiudad(lugar ?? "");
@@ -23,25 +28,24 @@ export default function LugarClima() {
         const fetchData = async () => {
             //const data = await obtenerClimaLugar(lugar, days);
             obtenerClimaLugar(lugar, days);
-
         };
 
         fetchData();
-    }, [lugar, handleChangeDias, searchParams, handleChangeCiudad, obtenerClimaLugar]);
+    }, [lugar, handleChangeDias, searchParams, handleChangeCiudad, obtenerClimaLugar, error, navigate]);
 
     return (
         <section className="w-full text-white">
-            {
-                !dataClima ? <LoaderGrid /> :
-                    <main className="w-full ">
-                        <div className="my-5 flex flex-col gap-4 justify-end items-end max-w-6xl mx-auto mb-5">
-                            <SearchClima />
-                        </div>
-                        <LocationInfoBar location={dataClima.location} />
-                        <ClimaLugarPage data={dataClima} />
-                    </main>
-            }
-
+            {!dataClima ? (
+                <LoaderGrid />
+            ) : (
+                <main className="w-full">
+                    <div className="mx-auto my-5 mb-5 flex max-w-6xl flex-col items-end justify-end gap-4">
+                        <SearchClima />
+                    </div>
+                    <LocationInfoBar location={dataClima.location} />
+                    <ClimaLugarPage data={dataClima} />
+                </main>
+            )}
         </section>
     );
 }
