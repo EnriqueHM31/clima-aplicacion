1823c86a151e2d9330952c28f64926792cfff1d1
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-14 20:30:32 -0600

    Refactor LugarClima component: enhance layout by adding a logo link and improving the structure of the main content area for better visual consistency and user experience.

--- src/pages/LugarClima.tsx
+++ src/pages/LugarClima.tsx
@@ -8,6 +8,9 @@ import ClimaLugarPage from "../components/ClimaPage/Sections/ClimaLugar";
 import SearchClima from "../components/Atomos/Search";
 import { useClimaStore } from "../store/Clima";
 import { toast } from "sonner";
+import ICONO from "/logo.ico";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function LugarClima() {
     const [searchParams] = useSearchParams();
@@ -39,10 +42,32 @@ export default function LugarClima() {
                 <LoaderGrid />
             ) : (
                 <main className="w-full">
-                    <div className="mx-auto my-5 mb-5 flex max-w-6xl flex-col items-end justify-end gap-4">
-                        <SearchClima />
+                    <div className="mx-auto flex w-full max-w-6xl items-center justify-between gap-4 px-4">
+                        <Link to="/" className="flex flex-1 items-center gap-4">
+                            <motion.picture
+                                className="flex size-12 items-center justify-center rounded-full bg-white object-cover p-2"
+                                initial={{ opacity: 0, x: -50 }}
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 0.8, ease: "easeIn" }}
+                            >
+                                <img src={ICONO} alt="logo" className="object-cover" />
+                            </motion.picture>
+                            <motion.h1
+                                className="text-2xl font-bold text-white"
+                                initial={{ opacity: 0, x: -50 }}
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 0.8, ease: "easeIn" }}
+                            >
+                                Aplicacion del clima
+                            </motion.h1>
+                        </Link>
+                        <div className="flex items-end justify-end gap-4 py-4">
+                            <SearchClima />
+                        </div>
                     </div>
-                    <LocationInfoBar location={dataClima.location} />
+                    <div className="mx-auto flex max-w-6xl gap-4 px-4">
+                        <LocationInfoBar location={dataClima.location} />
+                    </div>
                     <ClimaLugarPage data={dataClima} />
                 </main>
             )}
