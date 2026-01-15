0149ed5fd14baf05b26522ad01b41a8646651abe
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-14 23:11:27 -0600

    Actualizar URL en ServiceWetherApi: corregir parámetros de consulta para la API del clima, mejorando la precisión de las solicitudes.

--- src/services/climaService.ts
+++ src/services/climaService.ts
@@ -1,5 +1,5 @@
 export async function ServiceWetherApi(lugar: string, days: number) {
-    const url = `${import.meta.env.VITE_API}q=${lugar}&days=${days + 1}&aqi=yes&alerts=no`;
+    const url = `${import.meta.env.VITE_API}/${lugar}?days=${days + 1}`;
 
     const response = await fetch(url);
     const data = await response.json();
