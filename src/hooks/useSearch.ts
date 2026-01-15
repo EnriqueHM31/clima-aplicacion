1fe7205b575c5aa45ffb0f6bf83d6f20213623b0
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-14 21:15:44 -0600

    Add custom hook useSearch: implement navigation logic and error handling for city input, enhancing user experience and code organization.

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { useClimaStore } from "../store/Clima";
+
+export function useSearch() {
+    const { ciudad, dias, crearNewUrl, reset } = useClimaStore();
+    const navigate = useNavigate();
+
+    const handleNavigate = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (ciudad === "") {
+            reset();
+            toast.error("Error: Ingresa una ciudad");
+            return;
+        }
+        const UrlNew = crearNewUrl({ ciudad, dias });
+        navigate(UrlNew);
+        reset();
+    };
+
+    const OptionsSelect = Array.from({ length: 11 });
+
+    return {
+        handleNavigate,
+        OptionsSelect,
+    };
+}
