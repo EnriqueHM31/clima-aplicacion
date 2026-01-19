e2ec214bfb68c382373189fc46f3a86596da3443
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-19 13:27:09 -0600

    Actualizar NavBar: mejorar la disposición y responsividad del componente NavBar, añadiendo lógica condicional para mostrar SearchClima o LinkButton según la ruta actual.

--- src/components/Landing/NavBar.tsx
+++ src/components/Landing/NavBar.tsx
@@ -1,16 +1,23 @@
-import { FaGithub } from "react-icons/fa";
-import LinkButton from "../Atomos/LinkButton";
-import ICONO from "/logo.ico";
-import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { FaGithub } from "react-icons/fa";
+import { Link, useMatch } from "react-router-dom";
 import { buttonNavBarVariants } from "../../constants/animaciones";
+import LinkButton from "../Atomos/LinkButton";
+import SearchClima from "../Atomos/Search";
+import ICONO from "/logo.ico";
 
 export default function NavBar() {
+
+    const matchLugar = useMatch("/:lugar");
+
     return (
-        <nav className="mx-auto mb-4 flex w-full max-w-7xl flex-col items-center justify-between gap-4 py-1 md:flex-row">
-            <Link to="/" className="flex items-center gap-4">
+        <nav
+            className={`mx-auto mb-4 flex w-full max-w-6xl  items-center justify-between gap-4 py-1 md:flex-row
+            ${matchLugar ? "flex-col gap-8" : "flex-row gap-4"}
+        `}>
+            <Link to="/" className="flex items-center gap-2 md:gap-4">
                 <motion.picture
-                    className="flex size-12 items-center justify-center rounded-full bg-white object-cover p-2"
+                    className="flex size-8 md:size-12 items-center justify-center rounded-full bg-white object-cover md:p-2 p-1"
                     initial={{ opacity: 0, x: -50 }}
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.8, ease: "easeIn" }}
@@ -18,7 +25,7 @@ export default function NavBar() {
                     <img src={ICONO} alt="logo" className="object-cover" />
                 </motion.picture>
                 <motion.h1
-                    className="text-2xl font-bold text-white"
+                    className="text-lg md:text-2xl font-bold text-white"
                     initial={{ opacity: 0, x: -50 }}
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.8, ease: "easeIn" }}
@@ -26,7 +33,17 @@ export default function NavBar() {
                     Aplicacion del clima
                 </motion.h1>
             </Link>
-            <LinkButton text="Portafolio de proyectos HM" icon={<FaGithub />} variants={buttonNavBarVariants} />
+            {
+                matchLugar
+                    ? <div> <SearchClima /> </div>
+
+                    : <LinkButton
+                        text="Portafolio de proyectos HM"
+                        icon={<FaGithub className="size-5 text-white" />}
+                        variants={buttonNavBarVariants}
+                    />
+            }
+
         </nav>
     );
 }
