fafaefea43cbd2c9b9b81b4d448311efe1ed9971
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-20 13:36:39 -0600

    Agregar componente Footer en LugarClima y ajustar la estructura del layout para mejorar la disposición visual de la página.

--- src/pages/LugarClima.tsx
+++ src/pages/LugarClima.tsx
@@ -2,6 +2,7 @@
 import LoaderGrid from "../components/Atomos/Loading";
 import LocationInfoBar from "../components/ClimaPage/DataLocation";
 import ClimaLugarPage from "../components/ClimaPage/Sections/ClimaLugar";
+import Footer from "../components/Landing/Footer";
 //import { obtenerClimaLugar } from "../services/Clima";
 import NavBar from "../components/Landing/NavBar";
 import { useClima } from "../hooks/useClima";
@@ -10,19 +11,20 @@ export default function LugarClima() {
     const { dataClima } = useClima();
     return (
         <section className="w-full text-white">
+            <NavBar />
             {!dataClima ? (
                 <LoaderGrid />
             ) : (
                 <>
-                <NavBar />
-                <main className="w-full">
-                    <div className="mx-auto flex  gap-4 px-4">
-                        <LocationInfoBar location={dataClima.location} />
-                    </div>
-                    <ClimaLugarPage data={dataClima} />
-                </main>
+                    <main className="w-full mb-20">
+                        <div className="mx-auto flex  gap-4 px-4">
+                            <LocationInfoBar location={dataClima.location} />
+                        </div>
+                        <ClimaLugarPage data={dataClima} />
+                    </main>
                 </>
             )}
+            <Footer />
         </section>
     );
 }
