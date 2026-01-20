43c918841376735bf7e3921943b9fe4c696847dc
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-20 13:46:53 -0600

    Agregar fondo degradado en LugarClima: incluir un div con un fondo lineal y ajustar la clase del contenedor principal para mejorar la estética visual de la página.

--- src/pages/LugarClima.tsx
+++ src/pages/LugarClima.tsx
@@ -16,7 +16,9 @@ export default function LugarClima() {
                 <LoaderGrid />
             ) : (
                 <>
-                    <main className="w-full mb-20">
+                    <main className="w-full pb-30 relative h-full">
+                        <div className="bg-linear-to-t from-black/60 to-transparent to-40% w-full min-h-screen absolute bottom-0 left-0 -z-10 "></div>
+
                         <div className="mx-auto flex  gap-4 px-4">
                             <LocationInfoBar location={dataClima.location} />
                         </div>
