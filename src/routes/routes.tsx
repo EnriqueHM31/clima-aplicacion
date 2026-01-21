07f1f3d46d04df3b33259476d918c23e6f90ecf3
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-21 13:06:13 -0600

    Refactorizar enrutamiento de la aplicación: se implementó carga diferida para las páginas Home y LugarClima, y se agregó una ruta para manejar errores 404, mejorando la eficiencia y la experiencia del usuario al navegar por la aplicación.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,18 +1,23 @@
-import { useRoutes } from "react-router-dom";
-import HomePage from "../pages/Home";
-import LugarClima from "../pages/LugarClima";
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import LoaderGrid from "../components/Atomos/Loading";
+
+const HomePage = lazy(() => import("../pages/Home"));
+const LugarClima = lazy(() => import("../pages/LugarClima"));
+const NotFound404 = lazy(() => import("../pages/404"));
 
 export default function AppRouter() {
-    const routes = useRoutes([
-        {
-            path: "/",
-            element: <HomePage />,
-        },
-        {
-            path: "/:lugar",
-            element: <LugarClima />,
-        },
-    ]);
-
-    return routes;
+    return (
+        <Suspense fallback={
+            <div>
+                <LoaderGrid />
+            </div>}
+        >
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/:lugar" element={<LugarClima />} />
+                <Route path="*" element={<NotFound404 />} />
+            </Routes>
+        </Suspense>
+    )
 }
