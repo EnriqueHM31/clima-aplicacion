b57bd3c0a6f91d3fba19ef20511b432f9ee4eaa9
Luis Hernandez <luisenriquehernandezmarin0@gmail.com>
2026-01-20 23:00:49 -0600

    Eliminar log de consola en email.service: se ha removido la línea de console.log para limpiar el código y evitar la exposición de datos sensibles.

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -2,7 +2,6 @@ import { URL_EMAIL } from "../config";
 import { paginbaDondeEsEnviadoEmail } from "../constants";
 
 export async function enviarEmail({ email, mensaje }: { email: string, mensaje: string; }) {
-    console.log({ URL_EMAIL });
     const response = await fetch(URL_EMAIL, {
         method: "POST",
         headers: {
